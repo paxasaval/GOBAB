bfda1b88543f8aa89ab40b4a3eeb54e47c386e48
paxasaval <paxasaval1003@gmail.com>
2023-04-26 01:07:19 -0500

    fixed|forms comunication child-parent

--- src/app/shared/components/input-evidence1/input-evidence1.component.ts
+++ src/app/shared/components/input-evidence1/input-evidence1.component.ts
@@ -1,7 +1,14 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Evidence } from './../../../models/evidence';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { CharacteristicID } from 'src/app/models/characteristic';
 
+interface FormEvidence {
+  form: FormGroup,
+  evidence?: File,
+  flag: boolean,
+  changeName: string
+}
 @Component({
   selector: 'app-input-evidence1',
   templateUrl: './input-evidence1.component.html',
@@ -9,32 +16,105 @@ import { CharacteristicID } from 'src/app/models/characteristic';
 })
 export class InputEvidence1Component implements OnInit {
 
-  @Input() characteristic!:CharacteristicID
+  @Input() characteristic!: CharacteristicID
+  @Output() evidenceEmit = new EventEmitter<Evidence[]>()
+
+  fileFlag = true
+  inputChange = 'enlace'
+  inputChange1 = 'archivo'
+  pdfs: File[] = []
+  files: File[] = []
+  evidences: Evidence[] = []
 
   evidenceForm = new FormGroup({
-    observation:new FormControl(''),
-    link:new FormControl('')
+    observation: new FormControl(''),
+    link: new FormControl(''),
+    file: new FormControl(null)
   })
 
-  pdfs: File[] = []
-  files:File[] = []
-  getFiles(event:any){
-    this.files.push(event.target.files[0])
-    console.log(this.files)
+  arrayForms: FormEvidence[] = [
+    {
+      form: this.evidenceForm,
+      flag: this.fileFlag,
+      changeName: this.inputChange
+    }
+  ]
+
+  changeInput(form: FormEvidence) {
+    form.flag = !form.flag
+    if (form.changeName === this.inputChange) {
+      form.changeName = this.inputChange1
+
+    } else if (form.changeName === this.inputChange1) {
+      form.changeName = this.inputChange
+    }
+
+  }
+
+
+  getFiles(event: any, form: FormEvidence) {
+    const file = event.target.files[0] as File
+    form.evidence = file
+    this.emitEvidence()
+  }
+  addEvidence() {
+    const newForm = new FormGroup({
+      observation: new FormControl(''),
+      link: new FormControl(''),
+      file: new FormControl(null)
+    })
+    this.arrayForms.push({ form: newForm, flag: true, changeName: 'enlace' })
+  }
+
+  emitEvidence() {
+    this.evidences = []
+    this.arrayForms.forEach(formEvidence => {
+      if (formEvidence.evidence) {
+        const newEvidence: Evidence = {
+          characteristicID: this.characteristic.id,
+          name: formEvidence.evidence.name,
+          link: formEvidence.evidence,
+          note: formEvidence.form.get('observation')?.value,
+          subIndicatorID: '',
+          verified: false
+        }
+        this.evidences.push(newEvidence)
+      }
+      if (formEvidence.form.get('link')?.value != '') {
+        const newEvidence: Evidence = {
+          characteristicID: this.characteristic.id,
+          name: this.characteristic.name,
+          link: formEvidence.form.get('link')?.value,
+          note: formEvidence.form.get('observation')?.value,
+          subIndicatorID: '',
+          verified: false
+        }
+        this.evidences.push(newEvidence)
+      }
+    })
+    this.evidenceEmit.emit(this.evidences)
+
   }
 
   onDrop(event: any) {
-    console.log(event)
     const files: FileList = event.dataTransfer!.files;
     Array.from(files).forEach(file => {
       if (file.type === 'application/pdf') {
         this.pdfs.push(file);
+        //this.emitEvidence(file)
       }
     });
+
   }
 
   constructor() { }
 
+  changeObservation(event: any) {
+    this.emitEvidence()
+  }
+  changeLink(event: any) {
+    this.emitEvidence()
+  }
   ngOnInit(): void {
   }
 
