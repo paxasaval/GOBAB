6f54a1dffc5ce447899b31af45d84a1f8effe6e1
paxasaval <paxasaval1003@gmail.com>
2023-05-15 09:19:02 -0500

    interceptor

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -13,7 +13,8 @@ export const environment = {
   },
   production: false,
   API_URL:'http://localhost:3001',
-  ROL_ADMIN:'Administrador'
+  ROL_ADMIN:'Administrador',
+  TENATID:'64616f718e118fb22d4059e3'
 
   //API_URL:'https://iso-18091-backend-production.up.railway.app'
 };
